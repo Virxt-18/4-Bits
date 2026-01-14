b42e54c0eaf5e129833a6e10b4613a24fadefd11
Virxt-18 <demonvirat1980@gmail.com>
2026-01-14 12:30:02 +0530

    Installed lenis, added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,48 @@
-import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import Lenis from "@studio-freight/lenis";
+import { createContext, use, useContext, useEffect } from "react";
+export const LenisContext = createContext(null);
+
+export function useLenis() {
+  return useContext(LenisContext);
+}
 
 function App() {
 
+  useEffect(() => {
+    // disable browser scroll restore
+    if ('scrollRestoration' in window.history) {
+      window.history.scrollRestoration = 'manual';
+    }
+
+    window.scrollTo(0, 0);
+
+    const lenis = new Lenis({
+      duration: 1.2,
+      smoothWheel: true,
+      smoothTouch: false,
+    });
+
+    const raf = (time) => {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    };
+
+    requestAnimationFrame(raf);
+
+    return () => lenis.destroy();
+  }, []);
+
   return (
     <>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
